f24c4b5855bdf31a438916e21045c4af1caa1c1d
R-iu <cheuk0312@gmail.com>
2026-04-08 00:16:40 +0800

    add: update nvim plugins

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -1,20 +1,6 @@
 return {
 	"folke/snacks.nvim",
 	opts = {
-		-- dashboard = {
-		-- 	sections = {
-		-- 		{ section = "header" },
-		-- 		{ section = "keys", gap = 1, padding = 1 },
-		-- 		{ section = "startup" },
-		-- 		{
-		-- 			section = "terminal",
-		-- 			cmd = "ascii-image-converter ~/Pictures/wallpaper/skadi.jpg -C; sleep .1",
-		-- 			pane = 2,
-		-- 			indent = 4,
-		-- 			height = 30,
-		-- 		},
-		-- 	},
-		-- },
 		picker = {
 			hidden = true,
 			sources = {
@@ -29,6 +15,9 @@ return {
 		},
 		explorer = {
 			replace_netrw = true,
+		},
+		lazygit = {
+			configure = true,
 		},
 	},
 }
